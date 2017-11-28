c9bff979736cdbf2f5c41ce629cc191b24523d52
tiago.deliberali <tiago.deliberali@gmail.com>
2017-11-28 15:08:27 -0200

    added cursors

---
+++ Assets/Camera/CursorAffordance.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+    [SerializeField] Texture2D walkCursor;
+    [SerializeField] Texture2D targetCursor;
+    [SerializeField] Texture2D unknownCursor;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+
+    CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (cameraRaycaster.LayerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                break;
+        }
+    }
+}
