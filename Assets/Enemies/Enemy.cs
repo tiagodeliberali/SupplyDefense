2fd2b19a683c5d6c64bafc67acdd16eb78ac6a55
tiago.deliberali <tiago.deliberali@gmail.com>
2017-11-30 10:21:46 -0200

    added enemy behaviour

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,9 +1,63 @@
 ﻿using Assets.Utils;
 using System;
+using System.Linq;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 namespace Assets.Enemies
 {
     public class Enemy : BasePlayer
     {
+        [SerializeField] float attackDistance = 4f;
+        [SerializeField] float meleeDamage = 2f;
+
+        GameObject player;
+        GameObject house;
+        AICharacterControl ai;
+        Rigidbody rb;
+
+        private void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            house = SelectHouse();
+            ai = GetComponentInChildren<AICharacterControl>();
+            rb = GetComponentInChildren<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, rb.transform.position);
+
+            if (house == null)
+            {
+                house = SelectHouse();
+            }
+
+            if (distanceToPlayer <= attackDistance)
+            {
+                ai.SetTarget(player.transform);
+            }
+            else
+            {
+                ai.SetTarget(house.transform);
+            }
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            int hitLayer = collider.gameObject.layer;
+
+            var damageable = collider.gameObject.GetComponentInParent<IDamageable>();
+
+            if (damageable != null && hitLayer == (int)Layer.Player)
+            {
+                damageable.TakeDamage(meleeDamage);
+            }
+        }
+
+        private GameObject SelectHouse()
+        {
+            return GameObject.FindGameObjectsWithTag("House").FirstOrDefault();
+        }
     }
 }
